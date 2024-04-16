949265035ee6998a8d3168f84f7651fa22201332
rafatosta <rafa.ecomp@gmail.com>
2024-04-16 08:02:54 -0300

    :bookmark: 5.3

--- zapzap/__init__.py
+++ zapzap/__init__.py
@@ -1,6 +1,6 @@
 import os
 from PyQt6.QtCore import QStandardPaths
-__version__ = '5.2'
+__version__ = '5.3'
 __appname__ = 'ZapZap'
 __comment__ = 'WhatsApp Messenger for linux'
 __domain__ = 'com.rtosta'
@@ -22,7 +22,7 @@ __ddiHelper__ = 'https://www.ddi-ddd.com.br/Codigos-Telefone-Internacional/'
 
 __whatsapp_url__ = 'https://web.whatsapp.com/'
 # Link para pegar o userAgent: http://httpbin.org/user-agent
-__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+__user_agent__ = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
 
 # iniciando os paths
 from PyQt6.QtCore import QFileInfo
